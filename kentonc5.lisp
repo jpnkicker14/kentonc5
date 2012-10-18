a1f6294bec69ce0ab8c23ffbce6748a6d63bcc11
Kenton Chun <chunkenton1@gmail.com>
2012-10-17 23:57:53 -1000

    Updated code 10/17
    still needs comments on new code (macro)
    *NOTE: no new code for combining action

--- kentonc5.lisp
+++ kentonc5.lisp
@@ -1,26 +1,134 @@
-; Wizards_game part 1
+; Wizards_game from Land of Lisp
+; Assignment 5
+; Author: Kenton Chun
+; Author: Amy Takayesu
+; a lot of code comes from the Land of Lisp book
 
-(defparameter *ID* "Kenton Chun")
+(defparameter *ID* "Kenton Chun & Amy Takayesu")
 
+
+;allowed commands
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+;nodes for the scenery stored in a list
+(defparameter *nodes* '((living-room (you are in the living-room.
+                            a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                            there is a well in front of you.))
+                        (attic (you are in the attic.
+                            there is a giant welding torch in the corner.))
+			(bar (you are in a refurnished bar.
+                            there is a counter to your left.))))
+
+;edges will store the paths and the choices you have from that location
+(defparameter *edges* '((living-room (garden west door)  
+                                     (attic upstairs ladder)
+				     (bar south revolving-door))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))
+			(bar (living-room north revolving-door))))
+
+;objects on the ground parmater 
+(defparameter *objects* '(whiskey bucket frog chain coster glass vodka tonic-water lime))
+
+; the list of where the objects are located
+(defparameter *object-locations* '((whiskey living-room)
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)
+				   (coster bar)
+				   (glass attic)
+				   (vodka bar)
+				   (tonic-water living-room)
+				   (lime garden)))
+
+;the default location 
+(defparameter *location* 'living-room)
+
+;checks if chain has been welded to the bucket
+(defparameter *chain-welded* nil)
+
+
+
+;function that describes the location
+;this function will use the assoc to find the location based from the nodes
+;then cadr will return the description  
+(defun describe-location (location nodes)
+   (cadr (assoc location nodes)))
+
+;returns the descripiton based on the edge
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+;will call the describe path if there is more than one possible path
+;the map car will be applyed to every member of the list  
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+;the function that will return the objects at a given location
+(defun objects-at (loc objs obj-loc)
+   (labels ((is-at (obj)
+              (eq (cadr (assoc obj obj-loc)) loc)))
+       (remove-if-not #'is-at objs)))
+
+;function to describe the objects visible at a given location
+(defun describe-objects (loc objs obj-loc)
+   (labels ((describe-obj (obj)
+                `(you see a ,obj on the floor.)))
+      (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+;will return all of the parameters
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+;takes a direction and moves to that location if it's legal
+(defun walk (direction)
+  (labels ((correct-way (edge)
+             (eq (cadr edge) direction)))
+    (let ((next (find-if #'correct-way (cdr (assoc *location* *edges*)))))
+      (if next 
+          (progn (setf *location* (car next)) 
+                 (look))
+          '(you cannot go that way.)))))
+
+;tracks the location of objects 
+(defun pickup (object)
+  (cond ((member object (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object))
+	  (t '(you cannot get that.))))
+
+;keeps track of the objects that were picked up 
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+;used to check if you have an object
+(defun have (object) 
+    (member object (cdr (inventory))))
+
+;starts the special game REPL
 (defun game-repl ()
     (let ((cmd (game-read)))
         (unless (eq (car cmd) 'quit)
             (game-print (game-eval cmd))
             (game-repl))))
 
+;reads the commands 
 (defun game-read ()
     (let ((cmd (read-from-string (concatenate 'string "(" (read-line) ")"))))
          (flet ((quote-it (x)
                     (list 'quote x)))
              (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 
-(defparameter *allowed-commands* '(look walk pickup inventory))
-
+;calls the commands
 (defun game-eval (sexp)
     (if (member (car sexp) *allowed-commands*)
         (eval sexp)
         '(i do not know that command.)))
 
+;alters the output for game-print
 (defun tweak-text (lst caps lit)
   (when lst
     (let ((item (car lst))
@@ -32,114 +140,13 @@
             (caps (cons (char-upcase item) (tweak-text rest nil lit)))
             (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
 
+;prints the result in the custom REPL
 (defun game-print (lst)
     (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst)) 'list) t nil) 'string))
     (fresh-line))
 
-;nodes for the scenery stored in a list
-(defparameter *nodes* '((living-room (you are in the living-room.
-                            A Wizard is snoring loudly on the couch.))
-                        (garden (you are in a beautiful garden.
-                            there is a well in front of you.))
-                        (attic (you are in the attic.
-                            there is a giant welding torch in the corner.))
-			(well (you are in the well.
-			    there is a troll next to you.))	   
-			))
 
-;function that describes the location
-;this fucntion will use the assoc to find the location based from the nodes
-;then cadr will return the description  
-(defun describe-location (location nodes)
-  "describes the location based on a node"
-   (cadr (assoc location nodes)))
-
-;edges will store the paths and the choices you have from that location
-(defparameter *edges* '((living-room (garden west door)  
-                                     (attic upstairs ladder))
-                        (garden (living-room east door)
-				(well down rope))
-                        (attic (living-room downstairs ladder))
-			(well (garden up rope))
-			))
-
-;returns the descripiton based on the edge
-(defun describe-path (edge)
-  "describes the path based on the edge"
-  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
-
-;will call the describe path if there is more than one possible path
-;the map car will be applyed to every member of the list  
-(defun describe-paths (location edges)
-  "calls the describe path passing after finding the edges knowing the location"
-  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
-
-;objects on the ground parmater 
-(defparameter *objects* '(whiskey fabric bucket frog chain coin strap handle pockets))
-
-; the list of where the objects are located
-(defparameter *object-locations* '((whiskey living-room)
-				   (fabric living-room)
-				   (bucket living-room)
-                                   (chain garden)
-				   (strap garden)
-                                   (frog garden)
-				   (coin well)
-				   (handle well)
-				   (pockets attic)))
-
-;the function that will return the objects at a given location   
-(defun objects-at (loc objs obj-loc)
-  "return the objects at a location given the list of objects and object locations lists"
-   (labels ((is-at (obj)
-              (eq (cadr (assoc obj obj-loc)) loc)))
-       (remove-if-not #'is-at objs)))
-
-;function to describe the objects visible at a given location
-(defun describe-objects (loc objs obj-loc)
-  "returns the objects at a locaiton" 
-   (labels ((describe-obj (obj)
-                `(you see a ,obj on the floor.)))
-      (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
-
-;the default location 
-(defparameter *location* 'living-room)
-
-;will return all of the paramters
-(defun look ()
-  (append (describe-location *location* *nodes*)
-          (describe-paths *location* *edges*)
-          (describe-objects *location* *objects* *object-locations*)))
-
-;takes a direction and moves to that location if it's legal
-(defun walk (direction)
-  "moves the location after moving to a direciton"
-  (labels ((correct-way (edge)
-             (eq (cadr edge) direction)))
-    (let ((next (find-if #'correct-way (cdr (assoc *location* *edges*)))))
-      (if next 
-          (progn (setf *location* (car next)) 
-                 (look))
-          '(you cannot go that way.)))))
-
-;tracks the location of objects 
-(defun pickup (object)
-  "picks up the object and will keep track of it"
-  (cond ((member object (objects-at *location* *objects* *object-locations*))
-         (push (list object 'body) *object-locations*)
-         `(you are now carrying the ,object))
-	  (t '(you cannot get that.))))
-
-;keeps track of the objects that were picked up 
-(defun inventory ()
-  "displays the inventory of objects picked up"
-  (cons 'items- (objects-at 'body *objects* *object-locations*)))
-
-;used to check if you have an object
-(defun have (object) 
-  "returns if you have the object" 
-    (member object (cdr (inventory))))
-    (defmacro game-action (command subj obj place &body body)
+(defmacro game-action (command subj obj place &body body)
   `(progn (defun ,command (subject object)
             (if (and (eq *location* ',place)
                      (eq subject ',subj)
@@ -149,7 +156,6 @@
             '(i cant ,command like that.)))
           (pushnew ',command *allowed-commands*)))
 
-(defparameter *chain-welded* nil)
 
 (game-action weld chain bucket attic
              (if (and (have 'bucket) (not *chain-welded*))
@@ -172,3 +178,6 @@
                                    netherworlds- you lose! the end.))
                    (t '(the wizard awakens from his slumber and greets you warmly. 
                         he hands you the magic low-carb donut- you win! the end.))))
+
+
+
